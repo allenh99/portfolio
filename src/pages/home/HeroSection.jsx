aadeed72b832b9836547a551bda9e7aa959ca54a
Allen <allen@Kevins-MacBook-Pro-4.local>
2024-03-22 22:16:05 -0400

    first commit

---
+++ src/pages/home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+    return (
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">Allen H.</p>
+            <h1 className="hero--section--title">
+              <span className="hero--section-title--color">College Student</span>{" "}
+              <br />
+              Interested in CS
+            </h1>
+            <p className="hero--section-description">
+              this is filler text for now
+              <br /> also filler text
+            </p>
+          </div>
+          <button className="btn btn-primary">Get In Touch</button>
+        </div>
+        <div className="hero--section--img">
+        </div>
+      </section>
+    );
+  }
