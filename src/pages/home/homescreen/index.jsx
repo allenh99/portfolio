a8876f4b600085840535009129a54961f803690d
Allen <allen@Kevins-MacBook-Pro-4.local>
2024-03-31 16:56:03 -0400

    navbar,skills

--- src/pages/home/homescreen/index.jsx
+++ src/pages/home/homescreen/index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../HeroSection";
+import MySkills from "../Skills"
 
 export default function Home () {
     return(
         <>
         <HeroSection />
+        <MySkills />
         </>
     )
 }
